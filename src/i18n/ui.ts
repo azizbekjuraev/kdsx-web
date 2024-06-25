79e64c85838968565fe9d3b3cfbcdf0bf83ae61c
AZIZBEKJURAEV <ajuraev98@mail.ru>
2024-06-25 11:56:50 +0500

    blog translations

---
+++ src/i18n/ui.ts
@@ -0,0 +1,19 @@
+export const languages = {
+  en: "English",
+  uz: "Uzbek",
+  ru: "Russian",
+};
+
+export const defaultLang = "en";
+
+export const ui = {
+  en: {
+    published_on: "posted",
+  },
+  uz: {
+    published_on: "nashr qilindi",
+  },
+  ru: {
+    published_on: "опубликовано",
+  },
+} as const;
