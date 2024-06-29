551442bd8442b2873e03cfb3e88bf93515df8076
AZIZBEKJURAEV <ajuraev98@mail.ru>
2024-06-30 01:07:13 +0500

    fixing ui

--- public/js/index.js
+++ public/js/index.js
@@ -51,8 +51,3 @@ window.addEventListener("click", (event) => {
     iFrame.src = iFrame.src;
   }
 });
-
-AOS.init({
-  duration: 800, // Animation duration in milliseconds
-  offset: 100, // Offset (in pixels) from the top of the page
-});
